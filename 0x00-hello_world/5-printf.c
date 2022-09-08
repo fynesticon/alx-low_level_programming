7901b66b5c09497bb9c22807feae5fedac787608
Ekomobong Cletus <fynesticon@yahoo.com>
2022-09-08 09:56:59 -0700

    fixed task 4 - 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: prints "with proper grammar, but the outcome is a piece of
- *     * art,"
- *      *
- *       * Return: Always 0 (Success)
- *        */
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
