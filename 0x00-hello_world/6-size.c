709381f0f37e96d89d3bc984e8939965276206f3
Ekomobong Cletus <fynesticon@yahoo.com>
2022-09-08 09:59:37 -0700

    no 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));			
+	printf("Size of a float: %d byte(s)\n", sizeof(float));	
 	return (0);
 }
