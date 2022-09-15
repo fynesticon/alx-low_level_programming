ed266cde7d621de27f5aa601d244451891a2257c
Ekomobong Cletus <fynesticon@yahoo.com>
2022-09-15 15:31:37 -0700

    added task 0 to 10 of more function and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result (product of two integers)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
