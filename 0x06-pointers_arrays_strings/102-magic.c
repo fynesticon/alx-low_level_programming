6ef7bc15bb3ce3a072c9fe2532a1f857751a8a3f
Ekomobong Cletus <fynesticon@yahoo.com>
2022-09-21 16:22:35 -0700

    hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,16 +8,19 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
+/**
+ * You are not allowed to use the variable a in your new line of code
+ * You are not allowed to modify the variable p
+ * You can only write one statement
+ * You are not allowed to use ,
+ * You are not allowed to code anything else than the line of expected line of code at the expected line
+ * Your code should be written at line 19, before the ;
+ * Do not remove anything from the initial code (not even the comments)
+ * and don’t change anything but the line of code you are adding (don’t change the spaces to tabs!)
+ * You are allowed to use the standard library
+ */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
