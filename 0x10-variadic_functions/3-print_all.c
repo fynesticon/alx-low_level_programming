a940308fc931741feb865679b1d2c3bb7bac1511
Ekomobong Cletus <fynesticon@yahoo.com>
2022-10-13 16:28:43 -0700

    quick fix no 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,96 +1,47 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_c - print a char
- * @c: char to print
- *
- * Return: void
- */
-void print_c(va_list c)
-{
-	printf("%c", va_arg(c, int));
-}
-
-/**
- * print_s - prints a string
- * @s: string to print
- *
- * Return: void
- */
-void print_s(va_list s)
-{
-	char *str = va_arg(s, char *);
-
-	if (str == NULL)
-		str = "(nil)";
-	printf("%s", str);
-}
-
-/**
- * print_i - prints an int
- * @i: int to print
- *
- * Return: void
- */
-void print_i(va_list i)
-{
-	printf("%d", va_arg(i, int));
-}
-
-/**
- * print_f - prints a float
- * @f: float to print
- *
- * Return: void
- */
-void print_f(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-
-/**
  * print_all - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: void
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	print_t p[] = {
-	{"c", print_c},
-	{"s", print_s},
-	{"i", print_i},
-	{"f", print_f},
-	{NULL, NULL}
-	};
+	int i = 0;
+	char *str, *sep = "";
 
-	va_list valist;
-	char *separator = "";
+	va_list list;
 
-	va_start(valist, format);
+	va_start(list, format);
 
-	i = 0;
-
-	while (format && format[i])
+	if (format)
 	{
-		j = 0;
-		while (p[j].t != NULL)
+		while (format[i])
 		{
-			if (*(p[j].t) == format[i])
+			switch (format[i])
 			{
-				printf("%s", separator);
-				p[j].f(valist);
-				separator = ", ";
+				case 'c':
+				printf("%s%c", sep, va_arg(list, int));
 				break;
-																		}
-			j++;
+				case 'i':
+				printf("%s%d", sep, va_arg(list, int));
+				break;
+				case 'f':
+				printf("%s%f", sep, va_arg(list, double));
+				break;
+				case 's':
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+				default:
+				i++;
+				continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		i++;
 	}
-
-	va_end(valist);
 	printf("\n");
+	va_end(list);
 }
