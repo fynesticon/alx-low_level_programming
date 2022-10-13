d9bbe17d661627e7255a71cb32eba9eb1cebd9cf
Ekomobong Cletus <fynesticon@yahoo.com>
2022-10-13 15:44:01 -0700

    added the advanced tasks and some minor fixes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,27 +1,28 @@
-#include "function_pointers.h" 
-  
- /** 
-  * int_index - earches for an integer 
-  * @array: array to search in 
-  * @size: size of the array 
-  * @cmp: pointer to the comparing function 
-  * 
-  * Return: index of the first element for which 
-  * the cmp function does not return 0, or -1 if no match is found 
-  * or size is negative 
-  */ 
- int int_index(int *array, int size, int (*cmp)(int)) 
- { 
-         int i; 
-  
-         if (array && cmp) 
-         { 
-                 for (i = 0; i < size; i++) 
-                 { 
-                         if (cmp(array[i]) != 0) 
-                                 return (i); 
-                 } 
-         } 
-  
-         return (-1); 
- }
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to serch through
+ * @size: size of array
+ * @cmp: function used to compare
+ *
+ * Return: first index for which cmp doesn't return 0, or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, r;
+
+	if (size > 0 && array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			r = cmp(array[i]);
+			if (r)
+				break;
+		}
+		if (i < size)
+			return (i);
+	}
+	return (-1);
+}
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,44 @@
-#include <stdio.h> 
- #include <stdlib.h> 
- #include "3-calc.h" 
-  
- /** 
-  * main - performs simple operations 
-  * @argc: number of arguments passed 
-  * @argv: array of pointers to arguments 
-  * 
-  * Return: always 0 
-  */ 
- int main(int argc, char *argv[]) 
- { 
-         int a, b, c; 
-         int (*f)(int, int); 
-  
-         if (argc != 4) 
-         { 
-                 printf("Error\n"); 
-                 exit(98); 
-         } 
-         a = atoi(argv[1]); 
-         b = atoi(argv[3]); 
-         f = get_op_func(argv[2]); 
-         if (f == NULL) 
-         { 
-                 printf("Error\n"); 
-                 exit(99); 
-         } 
-         c = f(a, b); 
-         printf("%d\n", c); 
-         return (0); 
- }
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+	return (0);
+}
