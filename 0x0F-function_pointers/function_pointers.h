ab10aa6711efbaae9ea72592c3ec65bd17c5fcca
Ekomobong Cletus <fynesticon@yahoo.com>
2022-10-13 15:07:27 -0700

    fixed the header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef FUNC_H 
-#define FUNC_H 
-#include <stdlib.h> 
-  
- void print_name(char *name, void (*f)(char *)); 
- void array_iterator(int *array, size_t size, void (*action)(int)); 
- int int_index(int *array, int size, int (*cmp)(int)); 
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
- #endif
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#include <stdlib.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
