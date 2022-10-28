eb20fabde71f27e1ae300fc12f4aa2d583087d23
Ekomobong Cletus <fynesticon@yahoo.com>
2022-10-28 01:39:02 +0100

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,39 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-<<<<<<< HEAD
-	unsigned int ui;
-	int len, base_two;
-
-	if (!b)
-		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-=======
 	int i;
 	unsigned int num;
 
 	num = 0;
 	if (!b)
-	return (NULL);
+		return (NULL);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
->>>>>>> a083e6823f364c0cd5f992a89d22dc66709701c1
 			return (0);
-		if (b[len] & 1)
-			ui += base_two;
 	}
-<<<<<<< HEAD
-
-	return (ui);
-=======
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		num <<= 1;
@@ -48,5 +26,4 @@ unsigned int binary_to_uint(const char *b)
 			num += 1;
 	}
 	return (num);
->>>>>>> a083e6823f364c0cd5f992a89d22dc66709701c1
 }
